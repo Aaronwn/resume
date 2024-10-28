ddd373a646554848b5ada19540b103c1b9246d52
wangneng <wangneng@51talk.com>
2024-10-28 17:57:17 +0800

    feat: 优化移动端展示

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -15,11 +15,11 @@ export function SocialLink({
     <li className={className}>
       <Link
         href={href}
-        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+        className="group flex items-center text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
       >
         <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
         {children && (
-          <span className="ml-4 truncate min-w-[140px] sm:min-w-fit">
+          <span className="ml-4 break-all">
             {children}
           </span>
         )}
